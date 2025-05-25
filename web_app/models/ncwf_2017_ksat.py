8da61bd8b0eed1ba912ee8b8ca55f6c6364abb84
Pieter-Jan Coenen <pieter-jan@codon.one>
2025-05-25 15:12:18 +0200

    Added components mapping

--- web_app/models/ncwf_2017_ksat.py
+++ web_app/models/ncwf_2017_ksat.py
@@ -7,5 +7,10 @@ class Ncwf2017Ksat(models.Model):
     category = models.CharField(max_length=20, blank=False, null=False)
 
     @property
+    def withdrawn_in_ncwf_2024(self):
+        # Returns true if there is a ncwf_2024_tks related to this ksat
+        return self.ncwf_2024_tks.exists()
+
+    @property
     def url(self):
         return f"https://csrc.nist.gov/projects/cprt/catalog#/cprt/framework/version/NICE_1_1_0/home?element={self.ncwf_id}&type=work_role"
--- web_app/models/ncwf_2024_tks.py
+++ web_app/models/ncwf_2024_tks.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from web_app.models import Ncwf2017Ksat
+
 
 class Ncwf2024Tks(models.Model):
     ncwf_id = models.CharField(max_length=10, blank=False, null=False)
@@ -8,3 +10,7 @@ class Ncwf2024Tks(models.Model):
     ncwf_2017_ksats = models.ManyToManyField(
         "Ncwf2017Ksat", blank=True, related_name="ncwf_2024_tks"
     )
+
+    def new_in_ncwf_2024(self):
+        # Returns true if no Ncwf2017Ksat with ncwf_id with the same ncwf_id exists
+        return not Ncwf2017Ksat.objects.filter(ncwf_id=self.ncwf_id).exists()
