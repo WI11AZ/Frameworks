ec1ee23c480b8483661d1449690d553d7d1e4d23
WI11AZ <william.snoeck@gmail.com>
2025-07-03 12:46:39 +0200

    Suppression du dossier Frameworks

--- Frameworks/web_app/models/dcwf_category.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class DcwfCategory(models.Model):
-    title = models.CharField(max_length=100, blank=True, null=True)
-    description = models.CharField(max_length=500, blank=True, null=True)
-    color = models.CharField(max_length=30, blank=True, null=True)
-    url = models.CharField(max_length=500, blank=True, null=True)
--- Frameworks/web_app/models/opm.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class Opm(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
